60ee30732b6f0c6460c71e4399317d3f93924ece
Jeeya-Inamdar <jeeyainamdar@gmail.com>
2026-01-08 14:04:38 +0530

    consignment dome

--- server/model/export/ExJobModel.mjs
+++ server/model/export/ExJobModel.mjs
@@ -1031,6 +1031,7 @@ const exportJobSchema = new mongoose.Schema(
     // Export Checklist Additional Fields - Missing Fields Added
     cha: {
       type: String,
+      default: "Suraj Forwarders",
       trim: true,
     },
     masterblno: { type: String, trim: true }, // Master BL Number
